ff98015b3e5f5f2dd78a3f6f5a60a1db1b8a49e5
resan751 <indahmonte05@gmail.com>
2025-03-27 17:26:06 +0700

    finishing

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let luasSection = document.getElementById("luas");
-    let kelilingSection = document.getElementById("keliling");
-    let btnLuas = document.getElementById("btnLuas");
-    let btnKeliling = document.getElementById("btnKeliling");
+window.addEventListener('scroll', function() {
+    // parallax
+    let welcome = document.getElementById("head");
+    let intro = document.getElementById("intro");
 
-    // Tampilkan "Luas Persegi" secara default, sembunyikan "Keliling Persegi"
-    luasSection.style.display = "block";
-    kelilingSection.style.display = "none";
+    let value = window.scrollY;
+    let scaleValue1 = Math.max(1 - value * 0.001, 0.3);
+    welcome.style.bottom = value * 0.4 + 'px';
+    welcome.style.transform = `scale(${scaleValue1})`;
+    intro.style.bottom = value * 0.4 + 'px';
+    intro.style.transform = `scale(${scaleValue1})`;
+})
+// hitung luas
+function hitungluas() {
+    var sisi = document.getElementById('luas-sisi').value;
+    var luas = sisi * sisi;
 
-    btnLuas.addEventListener("click", function () {
-        luasSection.style.display = "block";
-        kelilingSection.style.display = "none";
-    });
+    document.getElementById('luas').innerHTML = `L = ${sisi} x ${sisi}`;
+    document.getElementById('hasil-luas').innerHTML = `L = ${luas}`;
+}
+// hitung keliling
+function hitungkeliling() {
+    var sisi = document.getElementById('keliling-sisi').value;
+    var keliling = 4 * sisi;
 
-    btnKeliling.addEventListener("click", function () {
-        kelilingSection.style.display = "block";
-        luasSection.style.display = "none";
-    });
-});
+    document.getElementById('keliling').innerHTML = `K = 4 x ${sisi}`;
+    document.getElementById('hasil-keliling').innerHTML = `K = ${keliling}`;
+}
